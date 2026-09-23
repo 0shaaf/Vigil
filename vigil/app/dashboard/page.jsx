9bc14cdd400269e3a58e0cf01ae58ca9340a7d0c
0shaaf <randomuserebay@gmail.com>
2026-09-23 22:26:58 +0500

    Cleanup

--- vigil/app/dashboard/page.jsx
+++ vigil/app/dashboard/page.jsx
@@ -14,6 +14,5 @@ export default async function DashboardPage() {
     .select("*")
     .eq("usr_id", user.id);
 
-  console.log("Endpoint Responded : " , checkRequest);
   return <ConstellationWeb switches={switches || []} />;
 }
